ece2d3a23db3be68b0d3a06e298a1e91db560a4e
NicolasQR <71113823+NicolasQR@users.noreply.github.com>
2020-09-19 18:12:05 -0500

    Final version
    JavaDocs, best store and total to pay

--- Integradora/src/model/Operations.java
+++ Integradora/src/model/Operations.java
@@ -2,8 +2,12 @@ package model;
 import ui.*;
 
 public class Operations{
-		
-
+	
+	
+	/**
+	* <b>Pre: </b> Arrays must be initialized <br>
+	* <b>Post: </b> Will give the total cost of the materials 
+	*/
 	public static void costMaterials(double[] costHomecenter, double[] costFerreteriaCentro, double[] costFerreteriaBarrio, double[] ferreteriaCentro,
 	double[] homeCenter, double[] ferreteriaBarrio, double[] amount){
 		
@@ -21,5 +25,31 @@ public class Operations{
 		}
 		
 	}
+
+
+	/**
+	* <b>Pre: </b> int must be initialized <br>
+	* <b>Post: </b> This will give the cost of the domicilie
+	*/
+	public static int costDomicilie(int totalHC, int totalFB, int totalFC ){
+		
+		int costDomicilie = 0;
+		
+		if((totalHC + totalFB + totalFC) < 80000){	
+			costDomicilie = 120000;
+			
+		} else if(totalHC < 300000){
+			costDomicilie = 28000;
+			
+		} 
+
+		return costDomicilie;
+	}
 	
 }
+
+
+
+
+
+
